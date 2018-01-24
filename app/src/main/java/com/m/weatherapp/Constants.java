2f99666f3da4d7a39421bf7eafe699765a030669
mulukadan <kadan.md@gmail.com>
2018-01-24 13:06:30 +0300

    Initial commit

---
+++ app/src/main/java/com/m/weatherapp/Constants.java
@@ -0,0 +1,12 @@
+package com.m.weatherapp;
+
+/**
+ * Created by kadan on 1/24/18.
+ */
+
+public class Constants {
+    public static final String API_KEY = BuildConfig.API_KEY;
+    public static final String API_BASE_URL = "api.openweathermap.org/data/2.5/weather";
+    public static final String YOUR_QUERY_PARAMETER = "q"; //Example: "location"
+    public static final String API_KEY_QUERY_PARAMETER = "appkey";
+}
